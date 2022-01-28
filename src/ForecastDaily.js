3f854edb1292eb20a87359c726d76b622a77cc01
JesKattady <jeskattady98@gmail.com>
2022-01-28 18:22:57 +0000

    Added Onecall API

--- src/ForecastDaily.js
+++ src/ForecastDaily.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import ForecastDay from "./ForecastDay";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -6,7 +7,18 @@ import Col from "react-bootstrap/Col";
 import "./dailyForecast.css";
 
 export default function forecastDaily(props) {
+  function handleResponse(response) {
+    console.log(response);
+  }
+
   console.log(props);
+  let lon = props.lon;
+  let lat = props.lat;
+  let apiKey = "115c7b570e452f9a86cfdde576ef94e1";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(handleResponse);
+
   return (
     <div className="forecastDaily">
       <Container>
